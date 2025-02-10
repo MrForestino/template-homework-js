2a248ae9e6d9dd837c9c02fe98b7fd588c04c739
MrForestino <borv2006@gmail.com>
2025-02-10 11:01:04 +0100

    update lesson 10 11 12 js

--- lessons/lesson-10/js/lesson-10.js
+++ lessons/lesson-10/js/lesson-10.js
@@ -1,9 +1,7 @@
-console.log('Lesson-10 JS');
-
 //todo [1]
 console.log(
-	"%c [1] ",
-	"color: yellow; background-color: #2274A5",
+    "%c [1] ",
+    "color: yellow; background-color: #2274A5",
 );
 //? Напиши функцію вищого порядку яка буде виводити 
 //? на екран повідомлення “Hello World!”. 
@@ -12,14 +10,13 @@ console.log(
 //? - з використання анонімної стрілкової колбек-функції з неявним поверненням.
 //! Код виконаного завдання
 
-anonimFunction(() => console.log("Hello World"));
 console.log("--------------------------------------------------");
 
 
 //todo [2]
 console.log(
-	"%c [2] ",
-	"color: yellow; background-color: #2274A5",
+    "%c [2] ",
+    "color: yellow; background-color: #2274A5",
 );
 //? Створи стрілкову колбек-функцію generatesRandomNumber з неявним поверненням, 
 //? яка генерує та повертає випадкове число від min = 1 до max = 10.
@@ -41,8 +38,8 @@ console.log("--------------------------------------------------");
 
 //todo [3]
 console.log(
-	"%c [3] ",
-	"color: yellow; background-color: #2274A5",
+    "%c [3] ",
+    "color: yellow; background-color: #2274A5",
 );
 //? Створи стрілкову колбек-функцію generatesRandomNumber1to5 з неявним поверненням,
 //? яка генерує та повертає випадкове число від 1 до 5.
@@ -58,15 +55,13 @@ console.log(
 //? перетворення випадкового числа від 1 до 5 від функції convertSize.
 //! Код виконаного завдання
 
-
-
 console.log("--------------------------------------------------");
 
 
 //todo [4]
 console.log(
-	"%c [4] ",
-	"color: yellow; background-color: #2274A5",
+    "%c [4] ",
+    "color: yellow; background-color: #2274A5",
 );
 //? Напиши функцію, applyCallbackToEachElement яка приймає масив чисел і колбек-функцію.
 //? Функція повинна застосовувати колбек-функцію
@@ -74,18 +69,20 @@ console.log(
 //? що містить результати застосування колбек-функції до кожного елементу.
 //! Код виконаного завдання
 const applyCallbackToEachElement = (array, callback) => {
-	//todo: написати тіло функції
+    //todo: написати тіло функції
 };
 const array = [1, 2, 3, 4, 5];
-const squareCallback = ''; //todo: написати код функції
+const squareCallback = ""; //todo: написати код функції
 const result = applyCallbackToEachElement(array, squareCallback);
 console.log("result:", result); //! [1, 4, 9, 16, 25]
 
 console.log("--------------------------------------------------");
+
+
 //todo [5]
 console.log(
-	"%c [5] ",
-	"color: yellow; background-color: #2274A5",
+    "%c [5] ",
+    "color: yellow; background-color: #2274A5",
 );
 //? Розрахунок дисконтної ціни.
 //? Створіть функцію, яка розраховує та повертає
@@ -98,10 +95,43 @@ console.log(
 const price = 100;
 const discount = 10;
 const calculateDiscountedPrice = (price, discount, callback) => {
-	//todo: написати тіло функції
+    //todo: написати тіло функції
 };
 const showDiscountedPrice = ""; //todo: написати код функції
 const discountPrice = applyCallbackToEachElement(price, discount, showDiscountedPrice);
 console.log(discountPrice); //! Discount price: 90
 
 console.log("--------------------------------------------------");
+
+//! ПРАКТИКА-10 (Урок-10_JS)
+//todo [Додаткове 10-1]
+console.log(
+    "%c [Додаткове 10-1] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Замінити в функції "Приготування меленої кави"
+//? функцію "Приготування меленої кави" та
+//? функцію "Приготування окропу (гарячої води)"
+//? на стрілкові функції та використати їх як колбеки.
+//todo Приклад-алгоритм приготування кави за допомогою функції "Приготування кави"
+//todo з використанням стрілкових функцій як колбеки.
+//! Код виконаного завдання
+
+console.log("-----------------------------------------");
+
+
+//todo [Додаткове 10-2]
+console.log(
+    "%c [Додаткове 10-2] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Замінити в функції "Приготування меленої кави"
+//? функцію "Приготування меленої кави" та
+//? функцію "Приготування окропу (гарячої води)"
+//? на анонімні стрілкові функції з неявним поверненням
+//? та використати їх як колбеки.
+//todo Приклад-алгоритм приготування кави за допомогою функції "Приготування кави"
+//todo з використання анонімних стрілкових функції з неявним поверненням як колбеки.
+//! Код виконаного завдання
+
+console.log("-----------------------------------------");
